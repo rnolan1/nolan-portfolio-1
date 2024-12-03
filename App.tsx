30a74304f52b8e04baa91ff78d039e8a734a6c53
rnolan1 <24007374@studentmail.ul.ie>
2024-12-03 13:07:16 -0600

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import Logo from './assets/Nolan_Logo.svg';
 import IG from './assets/ig-icon-blk.svg';
 import LI from './assets/li-icon-blk.svg';
-import BG from './assets/STL_IllustratedBG_BW2.png';
+import Git from './assets/github-mark.svg';
 import './App.css';
 import { Link, Routes, Route } from 'react-router-dom';
 import About from './pages/About';
@@ -9,12 +9,18 @@ import Work from './pages/Work';
 import Contact from './pages/Contact';
 import Home from './pages/Home';
 import Blog from './pages/Blog';
+import Resume from './assets/RNolanResume_2024_Web.pdf'
 
 function App() {
   return (
     <>
-      <nav>
+      {/* Navbar */}
+        <nav>
         <ul className="topnav">
+          <li><Link to="/Home"><img src={Logo} alt="Logo" /></Link></li>
+          <li className="resume-button">
+            <a href= {Resume} target="blank">Resume</a>
+          </li>
           <li><Link to="/Home">Home</Link></li>
           <li><Link to="/Work">Work</Link></li> 
           <li><Link to="/Contact">Contact</Link></li>
@@ -22,44 +28,30 @@ function App() {
           <li><Link to="/Blog">Blog</Link></li>
         </ul>
       </nav>
-
-      <Routes>
+        <Routes>
         <Route path="/Home" element={<Home />} />
         <Route path="/About" element={<About />} />
         <Route path="/Contact" element={<Contact />} />
         <Route path="/Blog" element={<Blog />} />
         <Route path="/Work" element={<Work />} />
       </Routes>
-
-      <div className="Logo">
-        <Link to="/Home"><img src={Logo} alt="Logo" /></Link>
-      </div>
-      <div>
-        <h4>Welcome to the Portfolio Site of</h4>
-        <h1>Rachel Nolan</h1>
-        <h4>Motion Designer | Artist</h4>
-      </div>
-      <div className="Video">
-        video will go here
-      </div>
-      <br />
-      <div className="footer-container">
+      
+      {/* Social Media Links */}
+      <footer className="social-media">
+        <p className='FooterTag'>let's create something amazing together!</p>
         <a href="https://www.linkedin.com/in/rachel-nolan-481490177/" target="_blank" rel="noopener noreferrer">
-          <img src={LI} alt="LinkedIn" />
+        <img src={LI} alt="LinkedIn" />
+        </a>
+        <a href="https://github.com/rnolan1" target="_blank" rel="noopener noreferrer">
+          <img src={Git} alt="GitHub" />
         </a>
         <a href="https://www.instagram.com/nolan.creative/" target="_blank" rel="noopener noreferrer">
           <img src={IG} alt="Instagram" />
         </a>
-      </div>
-      <div className="copyright">
-        <footer>©2024 Nolan Creative</footer>
-      </div>
-      <div className="footer-image">
-        <img src={BG} alt="Background" />
-      </div>
+      </footer>
     </>
   );
-}
+};
 
 export default App;
 
