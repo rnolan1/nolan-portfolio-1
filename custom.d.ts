d24298d3fa4a13abd162f1eaf3e7439ba128fd85
rnolan1 <24007374@studentmail.ul.ie>
2024-12-04 06:48:12 -0600

    Add files via upload

---
+++ custom.d.ts
@@ -0,0 +1,24 @@
+declare module '*.jpg' {
+    const value: string;
+    export default value;
+}
+
+declare module '*.png' {
+    const value: string;
+    export default value;
+}
+
+declare module '*.svg' {
+    const value: any;
+    export default value;
+}
+
+declare module '*.mp4' {
+    const value: string;
+    export default value;
+}
+
+declare module '*.pdf' {
+    const value: string;
+    export default value;
+}
